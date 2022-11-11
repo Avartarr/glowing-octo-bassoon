5df0b7b5ed04f21fdc0b724536453f6566519842
Avartarr <sunshineokoye6@gmail.com>
2022-11-11 14:48:57 +0100

    First commit

---
+++ Assignment.js
@@ -0,0 +1,30 @@
+a=7;
+b=2;
+c=6;
+d=0;
+e=4;
+if (a>b && a>c && a>d && a>e)
+{
+    console.log(a);
+    alert(a)
+}
+else if (b>a && b>c && b>d && b>e)
+{
+    console.log(b);
+    alert(b)
+}
+else if (c>a && c>b && c>d && c>e)
+{
+    console.log(c);
+    alert(c)
+}
+else if (d>a && d>b && d>c && d>e)
+{
+    console.log(d);
+    alert(d)
+}
+else
+{
+    console.log(e);
+    alert(e)
+}
